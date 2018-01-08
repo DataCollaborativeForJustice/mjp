0f3cb562ef9d19bdd7014f06784c1068db7de0aa
Tao Lin <tlin@jjay.cuny.edu>
2018-01-08 13:01:07 -0500

    adding read.mjp funtion from Adam, updating freqsum function

--- R/freqsum.r
+++ R/freqsum.r
@@ -1,9 +1,10 @@
 #' Frequency summary, a MJP team defined function
 #' 
 #' @param InputCol     Input column or vector, will be convert into factor.
-#' @param x number     of categories shown in the summary table, default = 20.
+#' @param x number of categories shown in the summary table, default = 20.
 #' @param sort_by      sort summary table by "VARIABLE" = category lable or "COUNT" = freqency
 #' @param sort_descending     show the result in acsending or descending order, default = TRUE, in descending order.
+#' @param big_mark adding seperator in the big numbers. if you dont like the scientific notation when using this on large numbers, please change your globle enviromental variable with 'option(scipen = 999)' .
 #' 
 #' @return     A frequency summary table with percentage.
 #' 
@@ -15,26 +16,58 @@ freqsum <-
     function(InputCol,
              x = 20,
              sort_by = "COUNT",
-             sort_descending = T) {
-        tmpvar <- data.frame(Freq = (summary(as.factor(InputCol), x)))
-        tmpvar <- cbind(TempVar = row.names(tmpvar), tmpvar)
-        row.names(tmpvar) <- c()
-        tmpvar$Percentage <-
-            paste(round(tmpvar$Freq / sum(tmpvar$Freq) * 100, 4), "%")
-        names(tmpvar) <- c("VARIABLE", "COUNT", "PERCENTAGE")
-        tmpvar_no_oth_na <-
-            tmpvar[!(tmpvar$VARIABLE %in% c("(Other)", "NA's")), ]
-        if (sort_by == "COUNT") {
-            tmpvar_no_oth_na <-
-                tmpvar_no_oth_na[order(as.numeric(tmpvar_no_oth_na[, sort_by]), decreasing = sort_descending), ]
-        } else{
-            tmpvar_no_oth_na <-
-                tmpvar_no_oth_na[order(as.character(tmpvar_no_oth_na[, sort_by]), decreasing = sort_descending), ]
+             sort_descending = T,
+             big_mark = NA) {
+        if (any(is.integer(InputCol),
+                is.numeric(InputCol))) {
+            tmp_sum <-
+                as.data.frame(unclass(summary(InputCol)))
+            tmp_sum[, 2] <- tmp_sum[, 1]
+            tmp_sum[, 1] <- rownames(tmp_sum)
+            names(tmp_sum) <-
+                c("Descriptive" , "Quantile")
+            out <- tmp_sum
         }
-        tmpvar_no_oth_na <- rbind(tmpvar_no_oth_na,
-                                  tmpvar[tmpvar$VARIABLE == "(Other)", ],
-                                  tmpvar[tmpvar$VARIABLE == "NA's", ])
-        tmpvar <- tmpvar_no_oth_na
-        row.names(tmpvar) <- c(1:nrow(tmpvar))
-        return(tmpvar)
+        else if (lubridate::is.POSIXct(InputCol)) {
+            tmp_sum <-
+                as.data.frame(unclass(summary(InputCol)))
+            tmp_sum[, 2] <- tmp_sum[, 1]
+            tmp_sum[, 1] <- rownames(tmp_sum)
+            tmp_sum[, 2] <-
+                as.POSIXct(tmp_sum[, 2], origin = ymd("1970-01-01"))
+            names(tmp_sum) <-
+                c("Descriptive" , "Quantile")
+            out <- tmp_sum
+        }
+        else{
+            tmpvar <- data.frame(Freq = (summary(as.factor(InputCol), x)))
+            tmpvar <- cbind(TempVar = row.names(tmpvar), tmpvar)
+            row.names(tmpvar) <- c()
+            tmpvar$Percentage <-
+                paste(round(tmpvar$Freq / sum(tmpvar$Freq) * 100, 4), "%")
+            names(tmpvar) <-
+                c("VARIABLE", "COUNT", "PERCENTAGE")
+            tmpvar_no_oth_na <-
+                tmpvar[!(tmpvar$VARIABLE %in% c("(Other)", "NA's")),]
+            if (sort_by == "COUNT") {
+                tmpvar_no_oth_na <-
+                    tmpvar_no_oth_na[order(as.numeric(tmpvar_no_oth_na[, sort_by]), decreasing = sort_descending),]
+            }
+            else{
+                tmpvar_no_oth_na <-
+                    tmpvar_no_oth_na[order(as.character(tmpvar_no_oth_na[, sort_by]),
+                                           decreasing = sort_descending),]
+            }
+            tmpvar_no_oth_na <- rbind(tmpvar_no_oth_na,
+                                      tmpvar[tmpvar$VARIABLE == "(Other)",],
+                                      tmpvar[tmpvar$VARIABLE == "NA's",])
+            tmpvar <- tmpvar_no_oth_na
+            if(!is.na(big_mark)){
+                tmpvar$COUNT <- format(tmpvar$COUNT, big.mark = big_mark)
+            }
+            row.names(tmpvar) <- c(1:nrow(tmpvar))
+            out <- tmpvar
+        }
+        
+        return(out)
     }
---
+++ R/read.mjp.R
@@ -0,0 +1,58 @@
+#' Read in a Data Set Encrypted with GPG, an MJP team defined function
+#' 
+#' @param file     Path to the data file encrypted with GNU Privacy Guard (GPG).  This currently works for CSV, SPSS, Stata, Rdata, and rds file types that have been ecrypted using GPG.
+#'
+#' @param tmp.path  Path to a directory where the data will be temporarily downloaded and decrypted.  WARNING -- This should not be a place on the cloud such as Dropbox; it should be a place on your local hard drive for security reasons.  The default is to create a temporary directory called "mjp.temp" in the user's Documents directory.  The user can specify a different directory in which to create the mjp.temp as a subdirectory.
+#' 
+#' @return  A data.frame of the data imported.
+#' 
+#' @examples
+#' ## IMPORT AN RDATA FILE TO DEFAULT TEMPORARY DIRECTORY
+#'     mydata <- read.mjp(file="~/Documents/dataset.Rdata.gpg")
+#'
+#' ## IMPORT AN RDATA FILE TO A USER-SPECIFIED TEMPORARY DIRECTORY
+#'     mydata <- read.mjp(file="~/Documents/dataset.Rdata.gpg", tmp.path="~/Data/")
+#' 
+#' @export
+######################################################################
+## COPIES ENCRYPTED FILES TO THE LOCAL HARD DRIVE IN A TEMPORARY
+## DIRECTORY;
+## DECRYPTS AND READS IN THE FILE;
+## REMOVES THE LOCAL COPIES AND THE TEMPORARY DIRECTORY;
+## WRITTEN BY ADAM G. FERA ON 2017-12-20
+######################################################################
+read.mjp <- function(file, tmp.path="~/Documents/") {
+    ## CREATE A NEW DIRECTORY IN WHICH TO SAVE FILES
+    tempDir <- paste0(tmp.path, "mjp.temp/")
+    dir.create(path=tempDir)
+    ## COPY FILE TO HARD DRIVE
+    file.copy(from=file, to=tempDir)
+    ## EXTRACT NAME OF ENCRYPTED FILE
+    crypt <- basename(path=file)
+    ## DECRYPT FILE
+    require(rcrypt)
+    decrypt(input=paste0(tempDir, crypt), verbosity=0)
+    ## EXTRACT NAME OF DECRYPTED FILE
+    decrypt <- gsub(pattern=".gpg", replacement="", x=crypt)
+    ## EXTRACT FILE EXTENSION OF DECRYPTED FILE
+    require(tools)
+    file.type <- tolower(file_ext(decrypt))
+    file.read <- paste0(tempDir, decrypt)
+    ## READ IN ENCRYPTED FILE AND ASSIGN TO OBJECT df
+    if (file.type=="csv") {
+        df <- read.csv(file=file.read)
+    } else if (file.type=="sav") {
+            require(foreign)
+            df <- read.spss(file=file.read, to.data.frame=TRUE)
+    } else if (file.type=="dta") {
+            require(haven)
+            df <- read_dta(file=file.read)
+    } else if (file.type=="rdata") {
+            df <- get(load(file=file.read))
+    } else if (file.type=="rds") {
+            df <- readRDS(file=file.read)
+    }
+    ## REMOVE ENCRYPTED AND DECRYPTED FILES FROM HARD DRIVE
+    unlink(x=tempDir, recursive=TRUE)
+    return(df)
+}
